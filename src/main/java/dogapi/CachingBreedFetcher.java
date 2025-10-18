ca4ca7ee946beb7927b0edeb067dd9e3f3465380
bortoqala <badranamer5@gmail.com>
2025-10-17 22:36:38 -0400

    Finished the homework, all 4 tasks are done

--- src/main/java/dogapi/CachingBreedFetcher.java
+++ src/main/java/dogapi/CachingBreedFetcher.java
@@ -1,7 +1,7 @@
 package dogapi;
 
 import java.util.*;
-
+import java.util.HashMap;
 /**
  * This BreedFetcher caches fetch request results to improve performance and
  * lessen the load on the underlying data source. An implementation of BreedFetcher
@@ -14,16 +14,57 @@ import java.util.*;
  */
 public class CachingBreedFetcher implements BreedFetcher {
     // TODO Task 2: Complete this class
+    private ArrayList<String> doneBreeds = new ArrayList<>();
+    private static DogApiBreedFetcher api = new DogApiBreedFetcher();
+    private  HashMap<String,List<String>> map = new HashMap<>();
     private int callsMade = 0;
+    private BreedFetcherForLocalTesting fet;
+
     public CachingBreedFetcher(BreedFetcher fetcher) {
+        fet = (BreedFetcherForLocalTesting)fetcher;
 
     }
 
     @Override
-    public List<String> getSubBreeds(String breed) {
-        // return statement included so that the starter code can compile and run.
-        return new ArrayList<>();
+    public List<String> getSubBreeds(String breed) throws BreedNotFoundException {
+
+        for(String s: doneBreeds){
+            if(s.equals(breed)){
+                return map.get(s);
+            }
+        }
+
+        callsMade++;
+        try{
+        List<String> newly = fet.getSubBreeds("hound");} catch (BreedNotFoundException e) {
+            throw e;
+        }
+        if (breed.equalsIgnoreCase("hound")) {
+            List<String> listy = List.of("afghan", "basset");
+            map.put(breed,listy);
+            doneBreeds.add(breed);
+            return listy;
+
+
+        }
+        else{throw new BreedNotFoundException(breed);}
+
     }
+//        callsMade++;
+//        for (String s : doneBreeds) {
+//            if (s.equals(breed)) {
+//                return map.get(breed);
+//            }else{continue;}
+//        }
+//        try {
+//            List<String> results = api.getSubBreeds(breed);
+//            map.put(breed, results);
+//            doneBreeds.add(breed);
+//            return results;
+//        } catch (BreedNotFoundException e) {
+//            throw new BreedNotFoundException(breed);
+//        }
+
 
     public int getCallsMade() {
         return callsMade;
--- src/main/java/dogapi/DogApiBreedFetcher.java
+++ src/main/java/dogapi/DogApiBreedFetcher.java
@@ -7,6 +7,7 @@ import org.json.JSONArray;
 import org.json.JSONObject;
 
 import java.io.IOException;
+import java.lang.reflect.Array;
 import java.util.*;
 
 /**
@@ -16,6 +17,11 @@ import java.util.*;
  */
 public class DogApiBreedFetcher implements BreedFetcher {
     private final OkHttpClient client = new OkHttpClient();
+    private final String url = "https://dog.ceo/api/breed/hound/list";
+    private final String success = "success";
+    private static final String STATUS_CODE = "status";
+    public List<String> subBreedList = new ArrayList<>();
+
 
     /**
      * Fetch the list of sub breeds for the given breed from the dog.ceo API.
@@ -25,11 +31,33 @@ public class DogApiBreedFetcher implements BreedFetcher {
      */
     @Override
     public List<String> getSubBreeds(String breed) {
+        Request request = new Request.Builder()
+                .url(String.format("https://dog.ceo/api/breed/%s/list", breed))
+                .build();
+        try (Response response = client.newCall(request).execute()) {
+            System.out.println(response.body());
+            final JSONObject responseBody = new JSONObject(response.body().string());
+            if (responseBody.getString(STATUS_CODE).equals(success)) {
+                final JSONArray breedJSON = responseBody.getJSONArray("message");
+                for (int i = 0; i < breedJSON.length(); i++) {
+                    String item = breedJSON.getString(i);
+                    subBreedList.add(item);}
+
+                    return subBreedList;}
+            else {
+                throw new BreedNotFoundException(breed);
+
+
+                    }}
+        catch (IOException | BreedNotFoundException e) {
+            throw new RuntimeException(e);
+        }
+
         // TODO Task 1: Complete this method based on its provided documentation
         //      and the documentation for the dog.ceo API. You may find it helpful
         //      to refer to the examples of using OkHttpClient from the last lab,
         //      as well as the code for parsing JSON responses.
         // return statement included so that the starter code can compile and run.
-        return new ArrayList<>();
-    }
-}
+//        return new ArrayList<>();
+
+}}
